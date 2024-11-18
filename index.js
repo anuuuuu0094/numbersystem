d1344733c00943c00a56f611a523ae6eb8133e74
anubhav maurya <channelsub47@gmail.com>
2024-11-18 18:14:44 +0530

    Code implement

---
+++ index.js
@@ -0,0 +1,16 @@
+// for prime numbers
+
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+  if(numbers[i]%2==0){
+    console.log(numbers[i])
+  };
+
+
+  // for odd numberes
+
+  if(numbers[i]%2 !=0){
+    console.log(numbers[i])
+  };
