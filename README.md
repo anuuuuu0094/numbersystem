0386e87b7c135c10436fd9b1d32d14556446f68c
anubhav maurya <channelsub47@gmail.com>
2024-11-18 18:28:21 +0530

    final code

---
+++ README.md
@@ -0,0 +1,11 @@
+Description :
+
+I am reating a repositoy that name is numberrsystem ;
+and coding according to assignment Question .
+using git command and push .
+
+even number = 2,20,14,12,8,6,,4,10,16,18
+
+odd number = 1,3,5,7,9,11,13,15,17,19
+
+prime number = 19,23,13,31,29,2,11,17,5
--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const numbers = [
     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
   ];
 
-  let sum = 0;
 
+let count = 0;
   if (numbers[i]%2===0){
-    sum++;
+    count ++;
   };
 
-  for(let i=1; i<=numbers.length; i++){
-    if (count == 2){
-        console.log(sum);
-    }
-  }
+if (count === 2){
+  console.log(numbers[i])
+};
